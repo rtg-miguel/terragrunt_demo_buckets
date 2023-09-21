67a60539cf158e28de77d47df27fec5bdf6df267
Miguel <jontiveros@roomstogo.com>
2023-09-21 03:03:04 -0400

    supporting labels

--- locals.tf
+++ locals.tf
@@ -1,3 +1,9 @@
 locals{
-  configuration = yamldecode(file(var.configuration_file))
+  decoded_configuration = yamldecode(file(var.configuration_file))
+  configuration = {for index, entry in local.decoded_configuration:
+    index => {
+      bucket = entry.bucket
+      tags = merge(var.labels, entry.tags)
+    }
+  }
 }
